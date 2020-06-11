87cdf67f7c4d150f9c7d3bc738ca25afa061e39d
Lee Langley <lee@greenimp.co.uk>
2020-06-11 17:30:44 +0100

    All exceptions take a `Throwable $previous` argument for exception chaining

--- src/Exceptions/InvalidMimeTypeException.php
+++ src/Exceptions/InvalidMimeTypeException.php
@@ -4,17 +4,25 @@ namespace GreenImp\PdfManager\Exceptions;
 
 use Exception;
 use Illuminate\Support\Arr;
+use Throwable;
 
 class InvalidMimeTypeException extends Exception
 {
-    public function __construct(string $mimeType = '', $allowedTypes = null)
+    /**
+     * InvalidMimeTypeException constructor.
+     *
+     * @param  string  $mimeType  The invalid mime type
+     * @param  array|null  $allowedTypes  [optional] The list of valid mime types
+     * @param  Throwable|null  $previous  [optional] The previous throwable used for the exception chaining.
+     */
+    public function __construct(string $mimeType = '', array $allowedTypes = null, Throwable $previous = null)
     {
-        $message = "Invalid mime type `{$mimeType}`";
+        $message = "Invalid mime type '{$mimeType}'";
 
         if (!empty($allowedTypes)) {
             $message .= '. One of "' . implode('", "', Arr::wrap($allowedTypes)) . '" expected';
         }
 
-        parent::__construct($message);
+        parent::__construct($message, 0, $previous);
     }
 }
